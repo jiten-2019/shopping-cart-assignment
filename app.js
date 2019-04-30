881c4620a8a023e08d631a300c400d36190d473e
Jitender Kumar <jitkumar10@publicisgroupe.net>
2019-04-30 12:18:15 +0530

    minor changes

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ var logger = require('morgan');
 var hbs = require('hbs');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
 var app = express();
 
@@ -34,7 +33,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
